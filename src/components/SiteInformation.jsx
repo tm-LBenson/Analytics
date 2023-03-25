6e64a55e97b43a42a5851880eb31712ca3581046
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-25 11:27:07 -0500

    add code block example to siteinformation

--- src/components/SiteInformation.jsx
+++ src/components/SiteInformation.jsx
@@ -1,6 +1,45 @@
-import React from 'react';
+// components/SiteInformation/index.js
+import React, { useState } from 'react';
+import { Icon } from '@iconify/react';
 
 const SiteInformation = () => {
+  const [copiedCode, setCopiedCode] = useState('');
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        setCopiedCode(text);
+        console.log('Code copied to clipboard');
+        setTimeout(() => setCopiedCode(''), 1500); // Reset the copied state after a delay
+      },
+      (err) => {
+        console.error('Could not copy the code: ', err);
+      },
+    );
+  };
+
+  const CodeBlock = ({ code }) => (
+    <span className="code-block">
+      {code}{' '}
+      {copiedCode === code ? (
+        <Icon
+          width="40"
+          icon="mdi:clipboard-check-multiple"
+          color="green"
+          onClick={() => copyToClipboard(code)}
+          className="clipboard-icon"
+        />
+      ) : (
+        <Icon
+          width="40"
+          icon="mdi:clipboard-multiple-outline"
+          onClick={() => copyToClipboard(code)}
+          className="clipboard-icon"
+        />
+      )}
+    </span>
+  );
+
   return (
     <div className="site-information">
       <h2>Welcome to My Site!</h2>
@@ -16,21 +55,40 @@ const SiteInformation = () => {
       <h3>How to use Analytics Benson in your React app</h3>
       <ol>
         <li>
-          Install the package: <code>npm install analytics-benson</code>
+          Install the package: <CodeBlock code="npm install analytics-benson" />
         </li>
         <li>
           Import the <code>analytics</code> function from the package:
           <br />
-          <code>import analytics from 'analytics-benson';</code>
+          <CodeBlock code="import analytics from 'analytics-benson';" />
         </li>
         <li>
           Call the analytics function, passing in your siteName and clientId:
           <br />
-          <code>analytics('Your Site Name', 'your-client-id');</code>
+          <CodeBlock code="analytics('Your Site Name', 'your-client-id');" />
         </li>
         <li>
           Place the function call in a suitable location in your app, such as in
-          the root component or a layout component.
+          the root component or a layout component. For example:
+          <br />
+          <pre className="code-block">
+            {`import React from 'react';
+import './App.css';
+import analytics from 'analytics-benson';
+
+function App() {
+  const CLIENT_ID = 'this-is-an-example-client-id'
+  analytics('Hello World App!', 'CLIENT_ID');
+
+  return (
+    <div className="App">
+      {/* Your main app content goes here */}
+    </div>
+  );
+}
+
+export default App;`}
+          </pre>
         </li>
       </ol>
     </div>
