84f72b88b25d9e41036a92753c2d824a561ba0d6
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-27 09:59:05 -0500

    import map icons

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+  iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+});
 
 const LocationMap = ({ ipAddresses }) => {
   const [locations, setLocations] = useState([]);
