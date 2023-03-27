7bfc5f98bd4a2619ed72d66dab46c04280bcdf08
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-27 10:04:16 -0500

    remove bugged part of map

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -1,14 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-  iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
-});
 
 const LocationMap = ({ ipAddresses }) => {
   const [locations, setLocations] = useState([]);
