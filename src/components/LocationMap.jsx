4089ba507de661d9dd78d5e35b75ab0a62df5886
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-27 10:09:51 -0500

    bugfix for map not showing marker

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 const LocationMap = ({ ipAddresses }) => {
   const [locations, setLocations] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
